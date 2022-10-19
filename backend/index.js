f956febe8a275c85cf8ff4a95ab76bb10b294a5e
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-10-19 14:39:01 +0100

    Set up express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`This app listening on port ${port}`));
