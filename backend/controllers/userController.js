352ff7b75efa730fb0321f6325224842f6ceff97
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-10-19 17:09:42 +0100

    Protect user routes & modify getMe function in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'get user data function'});
+    res.status(200).json(req.user)
 })
 
 // Generate JWT
