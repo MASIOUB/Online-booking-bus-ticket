57bfa103f8269912b9cf6cb26140210fd3b0631e
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-10-19 17:27:47 +0100

    add bus schema

---
+++ backend/models/busModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const busSchema = mongoose.Schema(
+    {
+        seats: {
+            type: Integer,
+            required: [true, 'Please add number of seats in this bus'],
+        },
+    },
+    { timestapms: true }
+)
+
+module.exports = mongoose.model('Bus', busSchema);
