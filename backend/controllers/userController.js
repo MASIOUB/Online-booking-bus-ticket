9fa8ba295c933b09e786f5d72d6c530c7e1da76e
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-10-19 16:32:11 +0100

    Express Async Handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,32 @@
-const { request } = require('express');
+const asyncHandler = require('express-async-handler');
+
 const User = require('../models/userModel');
 
 // @desc Register new user
 // @route Post /api/users
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     if (!req.body.name){
         res.status(400)
         throw new Error('Please fill all fields')
     }
 
     res.status(200).json({message: 'register user function'});
-}
+})
 
 // @desc Authenticate a user
 // @route Post /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'login user function'});
-}
+})
 
 // @desc Get user data
 // @route GET /api/users/me
 // @access Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'get user data function'});
-}
+})
 
 module.exports = {
     registerUser,
