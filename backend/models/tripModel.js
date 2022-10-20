dc36dfb6ea3d916b42f844a3a41680439133ced3
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-10-20 05:59:54 +0100

    add trip schema

---
+++ backend/models/tripModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const tripSchema = mongoose.Schema(
+    {
+        from: {
+            type: String,
+            required: [true, 'Please add the start station'],
+        },
+        to: {
+            type: String,
+            required: [true, 'Please add the return station']
+        },
+        date: {
+            type: Date,
+            required: [true, 'Please add the date'],
+        },
+        time: {
+            type: Time,
+            required: [true, 'Please add the time'],
+        },
+        price: {
+            type: String,
+            required: [true, 'Please add the price'],
+        },
+        bus: {
+            type: mongoose.Schema.Types.ObjectId,
+            reqired: true,
+            ref: "Bus",
+        }
+    },
+    { timestapms: true }
+)
+
+module.exports = mongoose.model('Trip', tripSchema);
