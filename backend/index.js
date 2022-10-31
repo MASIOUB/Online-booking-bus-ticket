e165bfaa65cc12e90ecec5913672dcdcddb7be89
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-10-31 09:26:23 +0100

    add cors package

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,16 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db');
+var cors = require('cors')
+
+
 
 const port = process.env.PORT || 5000;
 
 connectDB()
 
 const app = express();
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
