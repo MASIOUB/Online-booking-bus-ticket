3e05a7736f37c19ea8fef49c2026e2528d933f39
MASIOUB <93975488+MASIOUB@users.noreply.github.com>
2022-11-01 14:10:47 +0100

    add home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login'
 import Register from './pages/register'
 import Header from './components/Header';
+import Home from './pages/home';
+import Dashboard from './pages/dashboard';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Routes>
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
           </Routes>
         </div>
       </Router>
